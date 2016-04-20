e132147e07c5d744b6408d5737601d50a722f1ac
Moritz Schwörer <mr.mosch@gmail.com>
2016-04-20 10:35:13 +0200

    fixed name of command

--- Command/GenerateVersionCommand.php
+++ Command/GenerateVersionCommand.php
@@ -3,12 +3,10 @@ namespace AppBundle\Command;
 
 use Migration\VersionTransducer;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class GreetCommand extends Command
+class GenerateVersionCommand extends Command
 {
   protected function configure()
     {
